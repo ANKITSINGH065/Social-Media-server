ca293cb22ea55047691a36969625c73811cbbe63
Ankit singh <thakurbharat065@gmail.com>
2023-07-02 16:34:08 +0530

    change

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,6 +8,8 @@ export const isAuthenticated = asyncError(async (req, res, next) => {
 
   const { token } = req.cookies;
 
+  
+
   if (!token) return next(new ErrorHandler("Not Logged In", 401));
 
   const decodedData = jwt.verify(token, process.env.JWT_SECRET);
